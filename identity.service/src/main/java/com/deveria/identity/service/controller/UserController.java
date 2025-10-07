6b19edb1e4b1330bb6b2be6dc8461cde7fe45c36
adminmoi <pcanhtuandat@gmail.com>
2025-10-07 17:00:14 +0700

    Completed creating CRUD APIs

--- identity.service/src/main/java/com/deveria/identity/service/controller/UserController.java
+++ identity.service/src/main/java/com/deveria/identity/service/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.deveria.identity.service.controller;
 
 import com.deveria.identity.service.dto.request.UserCreateRequest;
+import com.deveria.identity.service.dto.request.UserUpdateRequest;
 import com.deveria.identity.service.entity.User;
 import com.deveria.identity.service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/users")
@@ -19,6 +19,28 @@ public class UserController {
     @PostMapping()
     User createUser(@RequestBody UserCreateRequest request) {
         return userService.createUser(request);
+    }
+
+    @GetMapping()
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PatchMapping("/{userId}")
+    User updateUser(@PathVariable("userId") String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable("userId") String userId){
+        userService.deleteUser(userId);
+
+        return "User deleted successfully";
     }
 
 }
--- identity.service/src/main/java/com/deveria/identity/service/service/UserService.java
+++ identity.service/src/main/java/com/deveria/identity/service/service/UserService.java
@@ -1,10 +1,13 @@
 package com.deveria.identity.service.service;
 
 import com.deveria.identity.service.dto.request.UserCreateRequest;
+import com.deveria.identity.service.dto.request.UserUpdateRequest;
 import com.deveria.identity.service.entity.User;
 import com.deveria.identity.service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -20,6 +23,31 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userRepository.save(user);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String userId){
+        return userRepository.findById(userId);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request){
+        User user = userRepository.findById(userId);
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId){
+        User user = userRepository.findById(userId);
+        userRepository.delete(user);
     }
 
 }
