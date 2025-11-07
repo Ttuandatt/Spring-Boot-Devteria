7608520293b956740030f9fa9ec02bb07e413219
adminmoi <pcanhtuandat@gmail.com>
2025-11-07 11:44:55 +0700

    - #19 làm quen với UnitTest

---
+++ identity.service/src/test/java/com/deveria/identity/service/controller/UserControllerTest.java
@@ -0,0 +1,100 @@
+package com.deveria.identity.service.controller;
+
+import com.deveria.identity.service.dto.request.UserCreateRequest;
+import com.deveria.identity.service.dto.response.UserResponse;
+import com.deveria.identity.service.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+
+    private UserCreateRequest request;
+    private UserResponse response;
+    private LocalDate dob;
+
+    @BeforeEach
+    void initData() {
+        dob = LocalDate.of(2000, 5, 4);
+        request = UserCreateRequest.builder()
+                .username("test4")
+                .password("12345")
+                .firstName("cu")
+                .lastName("loz")
+                .dob(dob)
+                .build();
+
+        response = UserResponse.builder()
+                .id("79161462-a2f0-4581-8e03-b8e6b9cc0c8d")
+                .username("test4")
+                .firstName("cu")
+                .lastName("loz")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        Mockito.when(userService.createUser(ArgumentMatchers.any()))
+                .thenReturn(response);
+
+        // WHEN - THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.id").value("79161462-a2f0-4581-8e03-b8e6b9cc0c8d"));
+    }
+
+    @Test
+    void createUser_invalidName_fail() throws Exception {
+        // GIVEN
+        request.setUsername("te"); // Expect to fail due to username being too short
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        Mockito.when(userService.createUser(ArgumentMatchers.any()))
+                .thenReturn(response);
+
+        // WHEN - THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1003))
+                .andExpect(MockMvcResultMatchers.jsonPath("message").value("Username must be between 3 and 20 characters"));
+    }
+}
---
+++ identity.service/src/test/java/com/deveria/identity/service/service/UserServiceTest.java
@@ -0,0 +1,84 @@
+package com.deveria.identity.service.service;
+
+import com.deveria.identity.service.dto.request.UserCreateRequest;
+import com.deveria.identity.service.dto.response.UserResponse;
+import com.deveria.identity.service.entity.User;
+import com.deveria.identity.service.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @MockitoBean
+    private UserRepository userRepository;
+
+
+
+    private UserCreateRequest request;
+    private UserResponse response;
+    private User user;
+    private LocalDate dob;
+
+
+    // Init test data before each test
+    @BeforeEach
+    void initData(){
+        dob = LocalDate.of(2000, 5, 4);
+        request = UserCreateRequest.builder()
+                .username("test4")
+                .password("12345")
+                .firstName("cu")
+                .lastName("loz")
+                .dob(dob)
+                .build();
+
+        response = UserResponse.builder()
+                .id("79161462-a2f0-4581-8e03-b8e6b9cc0c8d")
+                .username("test4")
+                .firstName("cu")
+                .lastName("loz")
+                .dob(dob)
+                .build();
+
+        user = User.builder()
+                .id(response.getId())
+                .username(request.getUsername())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .dob(request.getDob())
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_Success() {
+        // GIVEN
+        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        when(userRepository.save(any())).thenReturn(user);
+
+        // WHEN
+        var response = userService.createUser(request);
+
+        // THEN
+        assertThat(response.getId()).isEqualTo("79161462-a2f0-4581-8e03-b8e6b9cc0c8d");
+        assertThat(response.getUsername()).isEqualTo("test4");
+    }
+
+}
